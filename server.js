9029af26b7658cd6c3ca6f1375351d9f5051f4a0
LauraAy <laura.ay1564@gmail.com>
2020-03-03 22:06:47 -0800

    added gitignore file

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const { join } = require('path')
 const app = express()
 
-app.use(express.static(join(dirname, 'public')))
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
+// app.use(express.static(join(dirname, 'public')))
+// app.use(express.urlencoded({ extended: true}))
+// app.use(express.json())
 
 const { User, Recipe, RecipeCreator, Region} = require('./models')
 
