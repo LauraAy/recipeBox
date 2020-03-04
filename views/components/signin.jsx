5b7d9a854716cfaed3fc8c6b379d117fb46460ba
jlam <jennylamwork@gmail.com>
2020-03-04 14:29:30 -0800

    updated signin

--- views/components/signin.jsx
+++ views/components/signin.jsx
@@ -2,17 +2,28 @@ const React = require('react')
 
 const signIn = props => {
   return (
-    <form>
+  <form>
 
-      <div class="uk-margin">
-        <div class="uk-inline">
-          <span class="uk-form-icon" uk-icon="icon: user"></span>
-          <input class="uk-input" type="text" placeholder="Email">
+    <div class="uk-margin">
+      <div class="uk-inline">
+        <span class="uk-form-icon" uk-icon="icon: user"></span>
+        <input class="uk-input" type="text" placeholder="Email">
       </div>
-        </div>
-        <p uk-margin>
-          <a class="uk-button uk-button-default" href="#">Submit</a>
-        </p>
+    </div>
+
+    <div class="uk-margin">
+      <div class="uk-inline">
+        <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+        <input class="uk-input" type="text" placeholder="Password">
+      </div>
+    </div>
+    <p uk-margin>
+      <a class="uk-button uk-button-default" href="#">Sign In</a>
+    </p>
+    <p uk-margin>
+      <a class="uk-button uk-button-default" href="#">Register</a>
+    </p>
+
 
   </form>
   )
